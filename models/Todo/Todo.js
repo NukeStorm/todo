a691c749acfd3a3687433edc81eb6a3a05923188
NukeStorm <nsdm821@gmail.com>
2020-09-17 23:59:44 +0900

    Todo DTO 생성 db의 todo 테이블과 대응

---
+++ models/Todo/Todo.js
@@ -0,0 +1,10 @@
+class Todo {
+  constructor(id, title, content, date) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.date = date;
+  }
+}
+
+module.exports = Todo;
