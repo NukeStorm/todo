92e20925faea8f0647e770a24b69cbda61d9da65
NukeStorm <nsdm821@gmail.com>
2020-09-18 00:01:16 +0900

    TodoContainer Repository 구현 TodoContainer CRUD 담당

---
+++ models/TodoContainer/TodoContainerRepo.js
@@ -0,0 +1,50 @@
+const pool = require('../../DB/mysqPool');
+const TodoContainer = require('./TodoContainer');
+
+class TodoContainerRepo {
+  constructor() {
+    this.pool = pool;
+  }
+
+  async selectUserContainerIdList(userId) {
+    let connection = await this.pool.getConnection();
+    const [row, field] = await connection.query(`select * from container where user_id='${userId}'`);
+    connection.release();
+
+    return row;
+  }
+
+  async insertContainer(name, userid) {
+    let connection = await this.pool.getConnection();
+    let sql = `INSERT INTO todolist.container ( name,user_id)VALUES ('${name}','${userid}');`;
+    const [row, field] = await connection.query(sql);
+    connection.release();
+    return row;
+  }
+
+  async selectContainerTodoListById(id) {
+    let connection = await this.pool.getConnection();
+    const sql = `select todo.id,content,date,todo.user_id,container_id from todo,container where todo.container_id= container.id  and todo.container_id='${id}';`;
+    const [row, field] = await connection.query(sql);
+    connection.release();
+    return row;
+  }
+
+  async DeleteContainerById(id) {
+    let connection = await this.pool.getConnection();
+    const sql = `DELETE from container where id='${id}';`;
+    const [row, field] = await connection.query(sql);
+    connection.release();
+    return row;
+  }
+
+  async updateConainerOrder(id, order) {
+    let connection = await this.pool.getConnection();
+    const sql = `UPDATE container SET orderlist = ? WHERE id = ${id};`;
+    const [row, field] = await connection.query(sql, [order]);
+    connection.release();
+    return row;
+  }
+}
+
+module.exports = TodoContainerRepo;
