25040ff7b90109f2e41b6f5948720b8376c10cab
NukeStorm <nsdm821@gmail.com>
2020-09-23 00:02:42 +0900

    todo 및 todoContainer 초기화 방식 변경 추후 다시한번 변경 예정

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,14 +19,25 @@ function init(userid) {
 
       let board = document.querySelector('.board');
       data.forEach((container) => {
-        let todocontainer = Object.assign(new TodoContainer(), container);
+        container.todolist = container.todolist.map((todo) => {
+          todo = new Todo(todo.id, null, todo.content, todo.date, todo.user_id);
+          todo.render();
+          return todo;
+        });
+
+        let todocontainer = new TodoContainer(container.id, container.name, container.todolist);
+        todocontainer.render();
+        todocontainer.todolist.forEach((todo) => {
+          todo.setHandler(todocontainer);
+        });
+        console.log(todocontainer);
+        /*
         todocontainer.todolist = todocontainer.todolist.map((todo) => {
           todo = Object.assign(new Todo(), todo);
           todo.render();
           return todo;
         });
-
-        todocontainer.render();
+            */
 
         board.appendChild(todocontainer.node);
       });
