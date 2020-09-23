b35793422d087694c2241b7a3a5879df4a1cde0a
NukeStorm <nsdm821@gmail.com>
2020-09-23 09:32:10 +0900

    login rest api추가

--- routes/rest.js
+++ routes/rest.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
+const session = require('express-session');
 const TodoService = require('../service/TodoService');
+const UserService = require('../service/UserService');
 
 const router = express.Router();
 const todoService = new TodoService();
-
+const userService = new UserService();
 /* GET users listing. */
 router.get('/api/user', (req, res, next) => {
   res.json({ data: { user: 'hello', pw: 'test' } });
@@ -17,13 +19,56 @@ router.get('/api/todo/loadall/:userId', async (req, res, next) => {
   res.json({ data: result });
 });
 
-/* todo가 추가되었을때 */
-router.get('/api/todo/move/order', (req, res, next) => {
+/* todo 순서 바뀌었을때 업데이트 요청 */
+/*router.post('/api/todo/move/order', async (req, res, next) => {
+  let { orderlist } = req.body;
+  let containerId = req.body.container_id;
+  orderlist = orderlist.split(',').map((n) => parseFloat(n));
+  let result = await todoService.changeTodoOrder(containerId, orderlist);
+
+  console.log(result);
   res.json({ data: { user: 'hello', pw: 'test' } });
 });
-
+*/
 /* todo가  container를 바꿨을 때  */
 router.get('/api/todo/move/container', (req, res, next) => {
   res.json({ data: { user: 'hello', pw: 'test' } });
 });
+
+router.get('/api/user/login/:id/:pw', async (req, res) => {
+  const { id, pw } = req.params;
+  const sess = req.session;
+  const result = await userService.findUser(id, pw);
+
+  if (result) {
+    sess.id = result.id;
+
+    sess.save(() => {
+      console.log(sess.id);
+      res.json({ data: { res: true } });
+    });
+
+    return;
+  }
+  res.json({ data: { res: false } });
+});
+
+router.post('/api/user/login', async (req, res) => {
+  const { id, pw } = req.body;
+  const sess = req.session;
+  const result = await userService.findUser(id, pw);
+
+  if (result) {
+    sess.id = result.id;
+
+    sess.save(() => {
+      console.log(sess.id);
+      res.json({ data: { res: true, id: result.id } });
+    });
+
+    return;
+  }
+  res.json({ data: { res: false } });
+});
+
 module.exports = router;
